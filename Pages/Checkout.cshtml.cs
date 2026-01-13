f00c5b59a2cfa123a95eef0f0f401549dcd2421b
kabelo-sudo <kabelogivenesssosibo@gmail.com>
2026-01-13 09:47:41 +0200

    i added a code in checkout.cshtml.cs

--- Pages/Checkout.cshtml.cs
+++ Pages/Checkout.cshtml.cs
@@ -5,8 +5,23 @@ namespace OnlineShop.Pages
 {
     public class CheckoutModel : PageModel
     {
+        [BindProperty]
+        public string CustomerName { get; set; } = "";
+
+        [BindProperty]
+        public string CardNumber { get; set; } = "";
+
+        public string Message { get; set; } = "";
+
         public void OnGet()
         {
+            // Page load
+        }
+
+        public void OnPost()
+        {
+            // Fake payment logic
+            Message = "Payment Successful! Thank you for your order.";
         }
     }
 }
