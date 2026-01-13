6b2a5ebbb5bfe7c3ce7115bee5f6d17d5b506b9d
kabelo-sudo <kabelogivenesssosibo@gmail.com>
2026-01-13 09:18:43 +0200

    i've added code for products.cshtml and products.cshtml.cs folders

--- Pages/Products.cshtml.cs
+++ Pages/Products.cshtml.cs
@@ -1,12 +1,27 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OnlineShop.Data;
+using OnlineShop.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace OnlineShop.Pages
 {
     public class ProductsModel : PageModel
     {
+        private readonly ShopContext _context;
+
+        public List<Product> Products { get; set; } = new();
+
+        // Constructor: gets database connection
+        public ProductsModel(ShopContext context)
+        {
+            _context = context;
+        }
+
+        // Runs when page loads
         public void OnGet()
         {
+            Products = _context.Products.ToList();
         }
     }
 }
