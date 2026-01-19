cb2f22929bd8bf80ce9ec63b3403a482dba887c6
kabelo-sudo <kabelogivenesssosibo@gmail.com>
2026-01-19 10:41:40 +0200

    created new cartitem.cs model class

---
+++ Models/CartItem.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineShop.Models
+{
+    public class CartItem
+    {
+        public string Name { get; set; } = "";
+
+        public string Description { get; set; } = "";
+
+        public decimal Price { get; set; }
+
+        public string ImageUrl { get; set; } = "";
+    }
+}
