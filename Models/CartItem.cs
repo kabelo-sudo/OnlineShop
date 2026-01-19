350ff7135ef2cb38e3230aa6e1ae61d2002752d2
kabelo-sudo <kabelogivenesssosibo@gmail.com>
2026-01-19 11:50:55 +0200

    i created cartitem and added a code

--- Models/CartItem.cs
+++ Models/CartItem.cs
@@ -2,12 +2,11 @@
 {
     public class CartItem
     {
-        public string Name { get; set; } = "";
-
-        public string Description { get; set; } = "";
-
+        public int Id { get; set; } // optional, only if storing in DB
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = "";
         public decimal Price { get; set; }
-
+        public int Quantity { get; set; } = 1;
         public string ImageUrl { get; set; } = "";
     }
 }
